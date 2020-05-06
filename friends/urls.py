968f4ea4650ed7ac46c31be826400c58ee55b2e5
Sadman Hasan <sadman.h@live.com>
2020-05-06 06:55:44 -0400

    Friends class-based post, put, delete endpoints added

--- friends/urls.py
+++ friends/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
+
+from .models import Friends
 from .views import CountriesView, CitiesView, FriendsView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('countries', CountriesView)
 router.register('cities', CitiesView)
-router.register('friends', FriendsView)
+router.register('friends', FriendsView, basename='friends')
 
 urlpatterns = [
     path('', include(router.urls))
--- friends/views.py
+++ friends/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Countries, Cities, Friends
 from .serializers import CountriesSerializer, CitiesSerializer, FriendSerializer
 
@@ -18,3 +21,29 @@ class CitiesView(viewsets.ModelViewSet):
 class FriendsView(viewsets.ModelViewSet):
     queryset = Friends.objects.all()
     serializer_class = FriendSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        queryset = self.get_queryset()
+        serializer = FriendSerializer(queryset)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FriendSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, format=None):
+        queryset = self.get_queryset(pk=id)
+        serializer = FriendSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        queryset = self.get_object(pk=id)
+        queryset.delete()
+        return Response(status=status.HTTP_200_OK)
